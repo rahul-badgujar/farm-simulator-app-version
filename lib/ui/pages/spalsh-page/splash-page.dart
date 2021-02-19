4094d833516096a51642e219e25f14db6c3cef53
Rahul Badgujar <44890430+imRB7here@users.noreply.github.com>
2021-02-19 19:40:10 +0530

    google sign in added

--- lib/ui/pages/spalsh-page/splash-page.dart
+++ lib/ui/pages/spalsh-page/splash-page.dart
@@ -1,3 +1,5 @@
+import 'package:farm_game_app_version/services/firebase/auth/auth-helper.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class SplashPage extends StatefulWidget {
@@ -9,8 +11,44 @@ class _SplashPageState extends State<SplashPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Center(),
+      body: SizedBox(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            StreamBuilder<User>(
+              stream: AuthHelper().authStateChanges,
+              builder: (context, snapshot) {
+                if (snapshot.hasError) {
+                  return Text(snapshot.error.toString());
+                } else if (snapshot.connectionState ==
+                    ConnectionState.waiting) {
+                  return CircularProgressIndicator();
+                }
+                final user = snapshot.data;
+                if (user == null) {
+                  return Text("Not logged in");
+                } else {
+                  return Text("Logged in as: ${user.email}");
+                }
+              },
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                await AuthHelper().signInWithGoogle();
+              },
+              child: Text("Login using Google"),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                await AuthHelper().signOut();
+              },
+              child: Text("Logout"),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
