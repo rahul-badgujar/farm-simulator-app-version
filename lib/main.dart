4531111a50fae092f6663896039c3f8961a512da
Rahul Badgujar <44890430+imRB7here@users.noreply.github.com>
2021-02-19 20:39:18 +0530

    initialized flame game

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
+import 'package:farm_game_app_version/game/farm-game.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'app.dart';
+import 'package:flame/util.dart';
+import 'package:flutter/services.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(FarmGameApp());
+
+  final farmGame = FarmGame();
+  runApp(farmGame.widget);
+
+  Util flameUtil = Util();
+  await flameUtil.fullScreen();
+  await flameUtil.setOrientation(DeviceOrientation.portraitUp);
 }
